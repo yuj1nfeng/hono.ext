1fe47e54a59a177d5e98568b5c95be6fdb35ca87
yujf <296519653@qq.com>
2025-09-07 12:12:56 +0800

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ Hono.prototype.register = async function (root, prefix = '/api') {
         const file_path = path.join(file.parentPath, file.name);
         const dir_name = path.dirname(path.relative(root, file_path));
         const pkg = await import(file_path);
-        const api_path = `${prefix}/${dir_name}/${pkg.api_path}`;
+        const api_path = path.join(prefix, dir_name, pkg.api_path).replaceAll('\\', '/');
         this[pkg.method](api_path, pkg.app);
         console.log(`\x1b[32m load router: ${pkg.method}\t${api_path} ✔\x1b[0m `);
     }
