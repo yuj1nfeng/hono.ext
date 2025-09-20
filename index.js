b1ff08b957438a127bf0ccc666d6fe210b73e2e7
yujinfeng <yujf@yujinfengdeMac-mini.local>
2025-09-20 22:45:04 +0800

    dsasa

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ import { Hono } from 'hono';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
-
-
-
-
 Hono.prototype.register = async function (root, prefix = '/api') {
     const abs_root = path.isAbsolute(root) ? root : path.join(process.cwd(), root);
     const files = await fs.readdir(abs_root, { withFileTypes: true, recursive: true });
@@ -17,5 +13,23 @@ Hono.prototype.register = async function (root, prefix = '/api') {
         this[pkg.method](api_path, pkg.app);
         console.log(`\x1b[32m load router: ${pkg.method}\t${api_path} ✔\x1b[0m `);
     }
-}
+};
 
+export default class extends Hono {
+    constructor(...args) {
+        super(...args);
+    }
+
+    async register(root, prefix = '/api') {
+        const abs_root = path.isAbsolute(root) ? root : path.join(process.cwd(), root);
+        const files = await fs.readdir(abs_root, { withFileTypes: true, recursive: true });
+        for (const file of files.filter((p) => p.isFile())) {
+            const file_path = path.join(file.parentPath, file.name);
+            const dir_name = path.dirname(path.relative(root, file_path));
+            const pkg = await import(file_path);
+            const api_path = path.join(prefix, dir_name, pkg.api_path).replaceAll('\\', '/');
+            this[pkg.method](api_path, pkg.app);
+            console.log(`\x1b[32m load router: ${pkg.method}\t${api_path} ✔\x1b[0m `);
+        }
+    }
+}
